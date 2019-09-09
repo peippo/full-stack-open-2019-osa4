76f03fc2991dd6b44f1e0d232fa62c05d5399dbf
Janne Peippo <peippo@lumous.com>
2019-09-09 21:49:44 +0300

    Add osa 4a 4.5 answer

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,4 +1,4 @@
-const dummy = blogs => {
+const dummy = () => {
 	return 1;
 };
 
@@ -7,7 +7,19 @@ const totalLikes = blogs => {
 	return isNaN(total) ? 0 : total;
 };
 
+const mostLikes = blogs => {
+	let mostLiked = blogs[0];
+	blogs.map(blog => {
+		if (blog.likes >= mostLiked.likes) {
+			mostLiked = blog;
+		}
+	});
+
+	return mostLiked;
+};
+
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	mostLikes
 };
