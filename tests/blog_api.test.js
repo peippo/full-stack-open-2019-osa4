dbf2caa4974acb727c63262841996fb840778a30
Janne Peippo <peippo@lumous.com>
2019-09-24 20:11:31 +0300

    Add osa 4b 4.8 answer

---
+++ tests/blog_api.test.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app);
+
+test("Blogs are returned as json", async () => {
+	await api
+		.get("/api/blogs")
+		.expect(200)
+		.expect("Content-Type", /application\/json/);
+});
+
+test("Correct number of blogs returned", async () => {
+	const response = await api.get("/api/blogs");
+	expect(response.body.length).toBe(2);
+});
+
+afterAll(() => {
+	mongoose.connection.close();
+});
