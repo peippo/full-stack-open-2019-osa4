bb45289dd88ed7edb9db89a75f2e8bb9d3420f53
Janne Peippo <peippo@lumous.com>
2019-09-29 18:31:45 +0300

    Adjust tests & Jest config

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -25,7 +25,7 @@ const initialBlogs = [
 	}
 ];
 
-beforeEach(async () => {
+beforeAll(async () => {
 	await BlogPost.deleteMany({});
 
 	let blogObject = new BlogPost(initialBlogs[0]);
@@ -38,113 +38,124 @@ beforeEach(async () => {
 	await blogObject.save();
 });
 
-test("Blogs are returned as json", async () => {
-	await api
-		.get("/api/blogs")
-		.expect(200)
-		.expect("Content-Type", /application\/json/);
-});
+describe("API returns", () => {
+	test("blogs as json", async () => {
+		await api
+			.get("/api/blogs")
+			.expect(200)
+			.expect("Content-Type", /application\/json/);
+	});
 
-test("Correct number of blogs returned", async () => {
-	const response = await api.get("/api/blogs");
-	expect(response.body.length).toBe(3);
-});
+	test("correct number of blogs", async () => {
+		const response = await api.get("/api/blogs");
+		expect(response.body.length).toBe(3);
+	});
 
-test("ID field is named 'id'", async () => {
-	const response = await api.get("/api/blogs");
-	response.body.map(blog => {
-		expect(blog.id).toBeDefined();
+	test("ID field as 'id'", async () => {
+		const response = await api.get("/api/blogs");
+		response.body.map(blog => {
+			expect(blog.id).toBeDefined();
+		});
+	});
+
+	test("blogs with Likes field defined", async () => {
+		const response = await api.get("/api/blogs");
+		response.body.map(blog => {
+			expect(blog.likes).toBeGreaterThanOrEqual(0);
+		});
 	});
 });
 
-test("New blog posts can be added", async () => {
-	const newBlog = {
-		title: "React v16.9.0 and the Roadmap Update",
-		author: "Dan Abramov and Brian Vaughn",
-		url: "https://reactjs.org/blog/2019/08/08/react-v16.9.0.html",
-		likes: "0"
-	};
+describe("API allows", () => {
+	test("new blog posts to be added", async () => {
+		const newBlog = {
+			title: "React v16.9.0 and the Roadmap Update",
+			author: "Dan Abramov and Brian Vaughn",
+			url: "https://reactjs.org/blog/2019/08/08/react-v16.9.0.html",
+			likes: "0"
+		};
 
-	const initialResponse = await api.get("/api/blogs");
-	const blogsCount = initialResponse.body.length;
+		const initialResponse = await api.get("/api/blogs");
+		const blogsCount = initialResponse.body.length;
 
-	await api
-		.post("/api/blogs")
-		.send(newBlog)
-		.expect(201)
-		.expect("Content-Type", /application\/json/);
+		await api
+			.post("/api/blogs")
+			.send(newBlog)
+			.expect(201)
+			.expect("Content-Type", /application\/json/);
 
-	const response = await api.get("/api/blogs");
-	expect(response.body.length).toBe(blogsCount + 1);
+		const response = await api.get("/api/blogs");
+		expect(response.body.length).toBe(blogsCount + 1);
+	});
+
+	test("blog posts to be removed", async () => {
+		const newBlog = {
+			title: "React v16.9.0 and the Roadmap Update",
+			author: "Dan Abramov and Brian Vaughn",
+			url: "https://reactjs.org/blog/2019/08/08/react-v16.9.0.html",
+			likes: "0"
+		};
+
+		await api
+			.post("/api/blogs")
+			.send(newBlog)
+			.expect(201);
+
+		const response = await api.get("/api/blogs");
+		const blogId = response.body[response.body.length - 1].id;
+
+		await api.delete(`/api/blogs/${blogId}`).expect(204);
+	});
+
+	// FIXME: Returns 'null' on all fields?
+
+	// test("Blog posts can be modified", async () => {
+	// 	const newBlog = {
+	// 		title: "React v16.9.0 and the Roadmap Update",
+	// 		author: "Dan Abramov and Brian Vaughn",
+	// 		url: "https://reactjs.org/blog/2019/08/08/react-v16.9.0.html",
+	// 		likes: "0"
+	// 	};
+
+	// 	const updatedBlog = {
+	// 		title: "React v17.0.0 and the Roadmap Update",
+	// 		author: "Dan Abramov",
+	// 		url: "https://reactjs.org/blog/2019/08/08/react-v17.0.0.html",
+	// 		likes: "0"
+	// 	};
+
+	// 	await api
+	// 		.post("/api/blogs")
+	// 		.send(newBlog)
+	// 		.expect(201);
+
+	// 	let response = await api.get("/api/blogs");
+	// 	const blogId = response.body[response.body.length - 1].id;
+
+	// 	response = await api.put(`/api/blogs/${blogId}`, updatedBlog);
+	// 	expect(response.body.title).toBe("React v17.0.0 and the Roadmap Update");
+	// });
 });
 
-test("Likes field is defined", async () => {
-	const response = await api.get("/api/blogs");
-	response.body.map(blog => {
-		expect(blog.likes).toBeGreaterThanOrEqual(0);
+describe("API rejects", () => {
+	beforeEach(async () => {
+		await BlogPost.deleteMany({});
+	});
+
+	test("new blog posts without title or url fields", async () => {
+		const newBlog = {
+			author: "Dan Abramov and Brian Vaughn",
+			likes: "0"
+		};
+
+		await api
+			.post("/api/blogs")
+			.send(newBlog)
+			.expect(400);
 	});
 });
 
-test("New blog posts without title or url fields get rejected", async () => {
-	const newBlog = {
-		author: "Dan Abramov and Brian Vaughn",
-		likes: "0"
-	};
-
-	await api
-		.post("/api/blogs")
-		.send(newBlog)
-		.expect(400);
-});
-
-test("Blog posts can be removed", async () => {
-	const newBlog = {
-		title: "React v16.9.0 and the Roadmap Update",
-		author: "Dan Abramov and Brian Vaughn",
-		url: "https://reactjs.org/blog/2019/08/08/react-v16.9.0.html",
-		likes: "0"
-	};
-
-	await api
-		.post("/api/blogs")
-		.send(newBlog)
-		.expect(201);
-
-	const response = await api.get("/api/blogs");
-	const blogId = response.body[response.body.length - 1].id;
-
-	await api.delete(`/api/blogs/${blogId}`).expect(204);
-});
-
-// FIXME: Returns 'null' on all fields?
-
-// test("Blog posts can be modified", async () => {
-// 	const newBlog = {
-// 		title: "React v16.9.0 and the Roadmap Update",
-// 		author: "Dan Abramov and Brian Vaughn",
-// 		url: "https://reactjs.org/blog/2019/08/08/react-v16.9.0.html",
-// 		likes: "0"
-// 	};
-
-// 	const updatedBlog = {
-// 		title: "React v17.0.0 and the Roadmap Update",
-// 		author: "Dan Abramov",
-// 		url: "https://reactjs.org/blog/2019/08/08/react-v17.0.0.html",
-// 		likes: "0"
-// 	};
-
-// 	await api
-// 		.post("/api/blogs")
-// 		.send(newBlog)
-// 		.expect(201);
-
-// 	let response = await api.get("/api/blogs");
-// 	const blogId = response.body[response.body.length - 1].id;
-
-// 	response = await api.put(`/api/blogs/${blogId}`, updatedBlog);
-// 	expect(response.body.title).toBe("React v17.0.0 and the Roadmap Update");
-// });
-
-afterAll(() => {
+afterAll(async () => {
+	await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
 	mongoose.connection.close();
 });
