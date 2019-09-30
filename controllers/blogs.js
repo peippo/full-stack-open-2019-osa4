8d330af7ea295e963c45573e6158f352c6c80f28
Janne Peippo <peippo@lumous.com>
2019-09-30 21:23:54 +0300

    Add osa 4d 4.18 & 4.19 answers

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router();
 const BlogPost = require("../models/blogPost");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = request => {
+	const authorization = request.get("authorization");
+	if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+		return authorization.substring(7);
+	}
+	return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
 	const blogs = await BlogPost.find({}).populate("user", {
@@ -10,26 +19,39 @@ blogsRouter.get("/", async (request, response) => {
 	response.json(blogs.map(blog => blog.toJSON()));
 });
 
-blogsRouter.post("/", async (request, response) => {
+blogsRouter.post("/", async (request, response, next) => {
 	const body = request.body;
 
-	const user = await User.findById(body.userId);
+	const token = getTokenFrom(request);
 
-	const blog = new BlogPost({
-		title: body.title,
-		author: body.author,
-		url: body.url,
-		likes: body.likes === undefined ? 0 : body.likes,
-		user: user._id
-	});
+	try {
+		const decodedToken = jwt.verify(token, process.env.SECRET);
+		if (!token || !decodedToken.id) {
+			return response
+				.status(401)
+				.json({ error: "token missing or invalid" });
+		}
 
-	if (blog.title === undefined || blog.url === undefined) {
-		response.status(400).end();
-	} else {
-		const savedBlog = await blog.save();
-		user.blogs = user.blogs.concat(savedBlog._id);
-		await user.save();
-		response.status(201).json(savedBlog);
+		const user = await User.findById(decodedToken.id);
+
+		const blog = new BlogPost({
+			title: body.title,
+			author: body.author,
+			url: body.url,
+			likes: body.likes === undefined ? 0 : body.likes,
+			user: user._id
+		});
+
+		if (blog.title === undefined || blog.url === undefined) {
+			response.status(400).end();
+		} else {
+			const savedBlog = await blog.save();
+			user.blogs = user.blogs.concat(savedBlog._id);
+			await user.save();
+			response.status(201).json(savedBlog);
+		}
+	} catch (exception) {
+		next(exception);
 	}
 });
 
